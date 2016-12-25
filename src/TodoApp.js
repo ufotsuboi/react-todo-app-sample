5ec36acd7fb1290405f2a315417422d07627308c
yuuho.tsuboi <yuuho.tsuboi@adish.co.jp>
2016-12-26 07:34:18 +0900

    TodoApp作成

---
+++ src/TodoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="todoApp">
+        Hello React! I am a TODO Application.
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
